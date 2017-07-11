03e1481cbfa178bb85fe0dfa1f4a637782907dec
aolo2 <aolo2@yandex.ru>
2017-07-11 21:48:11 +0300

    Дижкстра работает??

--- dijkstra/main.cpp
+++ dijkstra/main.cpp
@@ -12,24 +12,21 @@
 
 typedef std::pair<unsigned long, float> vertex;
 
-void dijkstra(std::unordered_map<unsigned long, std::vector<vertex>> &graph, unsigned long source) {
-    struct vertex_comp {
-        bool operator()(const vertex &a, const vertex &b) { return a.second > b.second; }
-    };
+struct vertex_comp {
+    bool operator()(const vertex &a, const vertex &b) { return a.second > b.second; }
+};
 
+void dijkstra(std::unordered_map<unsigned long, std::vector<vertex>> &graph, unsigned long source) {
+    std::priority_queue<vertex, std::vector<vertex>, vertex_comp> Q;
+    std::unordered_set<unsigned long> q_set;
     std::unordered_map<unsigned long, float> dist;
     for (const auto &v : graph) { dist[v.first] = std::numeric_limits<float>::infinity(); }
 
-    std::priority_queue<vertex, std::vector<vertex>, vertex_comp> Q;
-    // dist[source] = 0.0f;
-
     Q.push({source, 0});
-
-    std::unordered_set<unsigned long> q_set;
-
     q_set.insert(source);
 
     while (!Q.empty()) {
+
         auto u = Q.top();
         Q.pop();
         q_set.erase(u.first);
@@ -41,18 +38,15 @@ void dijkstra(std::unordered_map<unsigned long, std::vector<vertex>> &graph, uns
         for (const auto &v : graph[u.first]) {
             float alt = dist[u.first] + v.second;
             if (alt < dist[v.first]) {
-                Q.push({v.first, alt});
-                q_set.insert(v.first);
+                if (q_set.find(v.first) == q_set.end()) {
+                    Q.push({v.first, alt});
+                    q_set.insert(v.first);
+                }
+
                 dist[v.first] = alt;
             }
         }
-
     }
-
-    for (const auto &entry : dist) {
-        std::cout << std::left << std::fixed << entry.first << ":" << entry.second << std::endl;
-    }
-
 }
 
 int main(int argc, char **argv) {
@@ -71,9 +65,10 @@ int main(int argc, char **argv) {
         weight = std::stof(line.substr(line.rfind(" ") + 1));
 
         graph[id].push_back({edge_to, weight});
+        graph[edge_to].push_back({id, weight});
     }
 
-    dijkstra(graph, 1);
+    dijkstra(graph, 450606976);
 
     return 0;
 }
