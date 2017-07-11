9e55073ff93b0ee2c7258f57af3ca43a74de749c
aolo2 <aolo@yandex.ru>
2017-07-11 12:58:18 +0300

    Фикс лексера + pqueue + dijkstra

--- dijkstra/main.cpp
+++ dijkstra/main.cpp
@@ -2,42 +2,13 @@
 // Created by aolo2 on 7/10/17.
 //
 
-#include <vector>
-#include <string>
+#include <unordered_map>
 #include <fstream>
-#include <iostream>
 #include <cmath>
 #include <map>
-#include <unordered_map>
+#include <queue>
 
-#include "min_pqueue.hpp"
-
-struct vertex {
-    unsigned long id;
-    float dist;
-
-    bool operator>(const vertex &other) const { return dist > other.dist; }
-
-    bool operator<(const vertex &other) const { return dist < other.dist; }
-
-    bool operator==(const vertex &other) const { return id == other.id; }
-};
-
-struct edge {
-    vertex target;
-    float weight;           // mb switch to int??
-};
-
-namespace std {
-
-    template<>
-    struct hash<vertex> {
-        std::size_t operator()(const vertex &v) const {
-            return v.id;
-        }
-    };
-
-}
+#include "min_pqueue/min_pqueue.hpp"
 
 bool relax(vertex from, vertex to, float weight) {
     bool changed = from.dist + weight < to.dist;
@@ -50,36 +21,15 @@ bool relax(vertex from, vertex to, float weight) {
 }
 
 // graph as adjacency list
-void dijkstra(std::map<vertex, std::vector<edge>> &graph,
+void dijkstra(std::map<unsigned long, std::vector<edge>> &graph,
               unsigned long source) {
 
-    norsi::min_pqueue<vertex> Q(graph.size());
-    Q.push({source, 0});
-
-    while (!Q.empty()) {
-        vertex u = Q.min_element();
-        Q.pop();
-
-        std::cout << graph[u].size() << std::endl;
-
-        for (auto &edge : graph[u]) {
-            vertex v = edge.target;    //FIXME: does this work? i.e. correct dist?
-
-            float alt = u.dist + edge.weight;
-            if (alt < v.dist) {
-                v.dist = alt;
-                //v.prev = u;
-                if (!Q.contains(v)) {
-                    Q.push(v);
-                }
-            }
-        }
-    }
+    // Dijkstra-NoDec
+    std::priority_queue Q;
 }
 
 int main(int argc, char **argv) {
-    // TODO: check usage
-
+/*
     std::string path(argv[1]);
     std::map<vertex, std::vector<edge>> graph;
     std::ifstream r(path);
@@ -96,15 +46,10 @@ int main(int argc, char **argv) {
         graph[{id, HUGE_VALF}].push_back({{edge_to, HUGE_VALF}, weight});
     }
 
-    for (const auto &p : graph) {
-        if (p.second.size() > 5) {
-            std::cout << p.first.id << std::endl;
-            break;
-        }
-    }
+*/
 
-    // second parameter is the source vertex
-    dijkstra(graph, 45060697);
+
+
 
     return 0;
 }
