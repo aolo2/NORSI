91c75a358fd6ead35268b875cff7fdf33fb81c98
Valentina <Valifirt@gmail.com>
2017-07-14 17:45:22 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,32 +1,66 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include<iterator>
 #include <fstream>
 #include <set>
 #include <cmath>
+#include <algorithm>
 
 #include "parser.h"
+#include "dijkstra.h"
 
-int main() {
+int main(int argc, char **argv) {
 
-    std::string path = "/home/valya/ClionProjects/NORSI_geo_2017/OSM_files/RU-TY.osm";
+    if (argc < 4){
+        std::cout << "Error arg" << std::endl;
+        exit(1);
+    }
 
-//    std::string path;
-//    std::cin >> path;
+    std::string path = argv[1];
 
     std::string name_file;
-    name_file = path.substr(path.rfind("/") + 1, path.rfind(".") - path.rfind("/") - 1);
-    std::ifstream f;
-    std::pair<std::map<unsigned long, Node>, std::map<unsigned long, std::vector<std::pair<unsigned long, float>>>> p;
+    name_file = path.substr(path.rfind("/") + 1, path.rfind(".") - path.rfind("/")-1);
 
-    f.open(path.substr(0, path.rfind("OSM") - 1) + "/graph/" + name_file + "_graph");
-    if (f) {
-        std::cout << "file graph is ready" << std::endl;
+    std::string work_dir = path.substr(0,path.rfind("/osm") + 1);
+
+    std::ifstream f;
+
+    std::pair<std::pair<std::map<unsigned int, Node>, std::map<std::string, unsigned int>>, std::unordered_map<unsigned int, std::vector<vertex>>>  p;
+
+    f.open(work_dir + "graph/" + name_file + "_graph");
+
+    if (f){
         p = parser_graph(f);
     } else {
         f.open(path);
-        p = parser_osm(f, name_file);
+        if (!f){
+            std::cout << "Can't find *.osm file" << std::endl;
+            exit(2);
+        }
+        p = parser_osm(f, work_dir, name_file);
     }
+
+    std::vector<unsigned int> route;
+
+    for(int i = 2; i < argc ; i++){
+        std::string point = argv[i];
+        if (p.first.second.find(point) == p.first.second.end()){
+            std::cout << "No point " << point << " in map" << std::endl;
+            exit(3);
+        }
+        route.push_back(p.first.second[point]);
+    }
+
+    std::pair<float, std::vector<unsigned int>> c;
+
+    for(int i = 0; i < route.size()-1; i++){
+        auto res = dijkstra(p.second, route[i], route[i+1]);
+        c.first += res.first;
+        std::copy(res.second.begin(), res.second.end(), back_inserter(c.second));
+    }
+
+    output_to_osc(c, p.first.first, work_dir);
 
     return 0;
 }
